8d328ca6ad230b9f72d27b1181bec2862e88b19c
shriyanshseluriyal <shriyanshseluriyal@gmail.com>
2026-09-21 13:09:02 +0530

    day1

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers \n");
+    scanf("%d %d",&a,&b);
+    printf("The sum is : %d",a+b);
+    printf("\nThe difference is : %d",a-b);
+    printf("\nThe product is : %d",a*b);
+    printf("\nThe quotient is : %d",a/b);
+}
